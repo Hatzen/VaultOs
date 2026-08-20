6ee9523a338dd622b344772ad917103b4a03d50a
Hatzen <kaihartz@gmx.net>
2026-08-20 20:58:16 +0200

    Fix build

--- packages/apps/Parannoying/Android.mk
+++ packages/apps/Parannoying/Android.mk
@@ -1,21 +1,17 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Parannoying
-LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := app-prod-release.apk
+
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_TAGS := optional
 
-# Der Trick: Wir setzen das Zertifikat explizit auf 'PRESIGNED' 
-# und verbieten dem Bausystem, die App während des Builds zu manipulieren
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REPLACE_PREBUILT_APK_INSTALLED := true
-
 LOCAL_PRODUCT_MODULE := true
 
-# Deaktiviert Dex-Optimierungen und Entpacken, da die App bereits fertig ist
-LOCAL_UNCOMPRESS_DEX := false
 LOCAL_DEX_PREOPT := false
 LOCAL_ENFORCE_USES_LIBRARIES := false
 
